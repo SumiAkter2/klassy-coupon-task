459c0c839288de7229304aeb2ea52cdcd325dd5a
SumiAkter2 <akteralisha36@gmail.com>
2023-08-02 15:00:39 +0600

    added react icons

--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -1,4 +1,7 @@
-import'./Header.css';
+import "./Header.css";
+import brandLogo from "../../assets/images/klassy-missy-logo.png";
+import signIn from "../../assets/images/sign_in_icon.png";
+import cart from "../../assets/images/carticon.png";
 const Header = () => {
   return (
     <div>
@@ -8,13 +11,32 @@ const Header = () => {
           Customer Care:Need Help ? Just Call - 01310202026 ( 10 AM to 6 PM
           only)
         </p>
-          </div>
-          
-          {/* middle header section */}
+      </div>
 
-          <div>
-              
-          </div>
+      {/* middle header section */}
+
+      <div className="flex justify-around items-center middle-header">
+        <div>
+          <a href="">
+            <img src={brandLogo} alt="brand-logo" />
+          </a>
+        </div>
+        <div>
+          <input className="" type="text" name="search" id="search" placeholder="Search for products" />
+        </div>
+        <div>
+          <ul className="flex justify-between items-center gap-5">
+            <li className="flex justify-between items-center gap-2">
+              <a href="">SIGN IN</a>
+              <img src={signIn} alt="Sign In logo" />
+            </li>
+            <li className="flex justify-between items-center gap-2">
+              <a href="">MY BAG</a>
+              <img src={cart} alt="cart logo" />
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
